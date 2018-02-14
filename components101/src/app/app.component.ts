b9597344364ba9ea9b834f0b38682abb22a87d23
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2018-02-14 13:58:35 -0200

    Aula 3

--- components101/src/app/app.component.ts
+++ components101/src/app/app.component.ts
@@ -2,9 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Hey Guys</h1>
+  <p>{{myObject.location}}</p>
+
+  <div *ngIf="myArr; then tpl1 else tpl2"></div>
+
+  <ng-template #tpl1>Yes</ng-template>
+  <ng-template #tpl2>No</ng-template>
+
+  `/*
+  <ul>
+    <li *ngFor="let arr of myArr">{{arr}}</li>
+    <li *ngIf="myArr; else otherTpl">Yeah, I exist.</li>
+  </ul>
+
+  <ng-template #otherTpl>No, I do.</ng-template>
+  */
+  ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  myObject = {
+    gender: 'male',
+    age: 33,
+    location: 'USA'
+  };
+
+  myArr = ['him', 'hers', 'yours'];
+
 }
